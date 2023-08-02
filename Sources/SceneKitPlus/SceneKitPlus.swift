0b3e068050e4ac1b0a5101b5666089b2e31d2444
Victor Vasconcelos <vfvasconcelos3010@gmail.com>
2023-08-02 12:16:34 -0400

    Testing

--- Sources/SceneKitPlus/SceneKitPlus.swift
+++ Sources/SceneKitPlus/SceneKitPlus.swift
@@ -2,5 +2,10 @@ public struct SceneKitPlus {
     public private(set) var text = "Hello, World!"
 
     public init() {
+        
+        let character = CharacterControlledEntity(characterData: .defaultCharacter, cameraNode: .init())
+        
+        print(character)
+        
     }
 }
