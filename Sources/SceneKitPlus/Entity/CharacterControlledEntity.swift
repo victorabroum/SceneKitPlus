f5f0d7cfb8d60fa2f841d74996094118eb727c23
Victor Vasconcelos <vfvasconcelos3010@gmail.com>
2023-08-17 15:06:29 -0400

    Improve the Character Controller Entity to be more generic and open

--- Sources/SceneKitPlus/Entity/CharacterControlledEntity.swift
+++ Sources/SceneKitPlus/Entity/CharacterControlledEntity.swift
@@ -8,7 +8,7 @@ import Foundation
 import GameplayKit
 import SceneKit
 
-public class CharacterControlledEntity: GKEntity {
+open class CharacterControlledEntity: GKEntity {
     
     public var controllerComp: CharacterControllerComponent?
     public var animStateComp: StateMachineComponent?
@@ -26,37 +26,57 @@ public class CharacterControlledEntity: GKEntity {
             let nodeComponent = GKSCNNodeComponent(node: scene.rootNode)
             self.addComponent(nodeComponent)
             
+            // Physics Component
             let physicsComp = PhysicsComponent(model: boydNode)
             self.addComponent(physicsComp)
             
+            // Camera Follow Component
             if (cameraFollowEnabled) {
                 self.addComponent(CameraFollowComponent(modelNode: modelNode, cameraNode: cameraNode))
             }
         }
         
         // Animation component
-        let animationComp = AnimationComponent(data: characterData)
+        setupAnimationComponent(data: characterData)
+        
+        // Animation State Component
+        setupAnimStateComp()
+        
+        // Move Component
+        setupMoveComponent(characterData: characterData)
+        
+        // Controller Component
+        setupControllerComponent(cameraNode: cameraNode)
+    }
+    
+    public func setupAnimStateComp() {
+        animStateComp = .init(stateMachine: .init(states: [
+            IdleState(entity: self, animationName: "idle"),
+            WalkState(entity: self, animationName: "walk", nextStates: [IdleState.self])
+        ]))
+        animStateComp?.stateMachine.enter(IdleState.self)
+    }
+    
+    public func setupAnimationComponent(data: CharacterData) {
+        let animationComp = AnimationComponent(data: data)
         self.addComponent(animationComp)
 
         animationComp.addAnimation(named: "walk", frameRange: 81...96)
         animationComp.addAnimation(named: "idle", frameRange: 0...80)
-        
-        animStateComp = .init(stateMachine: .init(states: [
-            IdleState(entity: self, animationName: "idle"),
-            WalkState(entity: self, animationName: "walk")
-        ]))
-        
+    }
+    
+    public func setupMoveComponent(characterData: CharacterData) {
         let moveComp = MovementComponent(speed: characterData.speed,
                                          jumpForce: characterData.jumpForce)
         self.addComponent(moveComp)
-        
-        controllerComp = CharacterControllerComponent(cameraNode: cameraNode)
-        self.addComponent(controllerComp!)
-        
-        
     }
     
-    required init?(coder: NSCoder) {
+    public func setupControllerComponent(cameraNode: SCNNode){
+        controllerComp = CharacterControllerComponent(cameraNode: cameraNode)
+        self.addComponent(controllerComp!)
+    }
+    
+    required public init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
