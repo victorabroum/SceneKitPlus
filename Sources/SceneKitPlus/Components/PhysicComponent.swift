8226a148c0bb2ebeb03fdc537880aba42249be30
Victor Vasconcelos <vfvasconcelos3010@gmail.com>
2023-09-06 16:44:03 -0400

    implement deinit on physics component

--- Sources/SceneKitPlus/Components/PhysicComponent.swift
+++ Sources/SceneKitPlus/Components/PhysicComponent.swift
@@ -32,4 +32,8 @@ public class PhysicsComponent: GKComponent {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    deinit {
+        self.model.physicsBody = nil
+    }
 }
